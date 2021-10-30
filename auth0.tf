b2d755187a365a0574c4b74f74903376c3dd03aa
Niels Sandholt Busch <niels.busch@gmail.com>
2021-10-30 23:42:09 +0200

    re-add client app

--- auth0.tf
+++ auth0.tf
@@ -40,3 +40,45 @@ resource "auth0_client_grant" "myjobplanner-api" {
   audience  = auth0_resource_server.myjobplanner-api.identifier
   scope     = ["create:business", "read:business", "update:business", "delete:business"]
 }
+
+resource "auth0_client" "myjobplanner-api-swagger-ui" {
+  name                                = "myJobPlanner Api - Swagger UI"
+  description                         = "myJobPlanner Api - Swagger UI"
+  app_type                            = "spa"
+  custom_login_page_on                = false
+  is_first_party                      = true
+  is_token_endpoint_ip_header_trusted = true
+  token_endpoint_auth_method          = "client_secret_post"
+  oidc_conformant                     = true
+  callbacks                           = ["https://api.myjobplanner.com/callback", "http://localhost:3000/callback"]
+  allowed_origins                     = ["https://api.myjobplanner.com", "http://localhost:3000"]
+  grant_types                         = ["authorization_code", "http://auth0.com/oauth/grant-type/password-realm", "implicit", "password", "refresh_token"]
+  allowed_logout_urls                 = ["https://api.myjobplanner.com", "http://localhost:3000"]
+  web_origins                         = ["https://api.myjobplanner.com", "http://localhost:3000"]
+  jwt_configuration {
+    lifetime_in_seconds = 300
+    secret_encoded      = true
+    alg                 = "RS256"
+    scopes = {
+      foo = "bar"
+    }
+  }
+  refresh_token {
+    rotation_type                = "non-rotating"
+    expiration_type              = "expiring"
+    leeway                       = 15
+    token_lifetime               = 1300000
+    infinite_idle_token_lifetime = true
+    infinite_token_lifetime      = false
+    idle_token_lifetime          = 1296000
+  }
+  client_metadata = {
+    app = "swagger"
+  }
+}
+
+resource "auth0_client_grant" "myjobplanner-api-swagger-ui" {
+  client_id = auth0_client.myjobplanner-api-swagger-ui.id
+  audience  = auth0_resource_server.myjobplanner-api.identifier
+  scope     = ["create:business", "read:business", "update:business", "delete:business"]
+}
